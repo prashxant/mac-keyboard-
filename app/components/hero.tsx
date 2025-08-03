8c62f795cbc1a9cadfbf9077528e1e4c4c78570c
prashxant <notxdead0@gmail.com>
2025-08-03 18:20:08 +0530

    debaug

---
+++ app/components/hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Linklogo  } from '../logos/linkin'
+import { GithubLogo } from '../logos/githubLogo'
+
+
+export const Hero = () => {
+  return (
+    <div className=' ml-50  mx-auto mt-20'>
+        <p className='text-5xl text-sky-200'>Not copied made by myself</p>
+        <div className="text-xl mt-2 text-sky-200"> nahi pata kya likhna h yaha par baas bhar diya</div>
+        <div className="flex text-zinc-300 mt-5 "> <Linklogo/><GithubLogo/></div>
+    </div>
+  )
+}
+
---
+++ app/components/keyboardbg.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Keyboard = () => {
+  return (
+    <div className='w-[90vw] mt-10  mx-auto'>
+      
+    </div>
+  )
+}
